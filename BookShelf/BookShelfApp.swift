e55345146e0e0dc9d074a14a9da4cddbc9e181c8
Vladimir <motiw@icloud.com>
2023-04-29 19:14:10 +0600

    Initial Commit

---
+++ BookShelf/BookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfApp.swift
+//  BookShelf
+//
+//  Created by Vladimir on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
